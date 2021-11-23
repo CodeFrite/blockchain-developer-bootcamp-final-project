1147e0552568d7cff36e5ad9a0225cf6f5ab2160
Hadi Sakali <hadi.sakali@gmail.com>
2021-11-23 15:30:45 +0100

    Move Escrow to Proxy / restrict instructions access to proxy / add corresponding tests

--- contracts/InstructionsProvider.sol
+++ contracts/InstructionsProvider.sol
@@ -13,10 +13,10 @@ contract InstructionsProvider is Ownable {
     /* STORAGE */
 
     /// @dev Interpreter contract reference
-    Interpreter private interpreterInstance;
+    Interpreter private interpreterContractRef;
 
     /// @dev OpenZeppelin Escrow contract reference
-    Escrow private escrow;
+    Escrow private escrowInstance;
 
     /* EVENTS */
     
@@ -26,97 +26,62 @@ contract InstructionsProvider is Ownable {
     * @param _old Old address of the Interpreter contract
     * @param _new New address of the Interpreter contract
     */
-    event SetInterpreterInstance(address _from, address _old, address _new);
+    event SetInterpreterContractRef(address _from, address _old, address _new);
 
-    // TODO: TESTING: remove this event
-    event _CALLBACK(address _from, address _to, uint _success);
+    /**
+    * @dev Event emitted when the Escrow contract reference is changed
+    * @param _from Caller address
+    * @param _old Old address of the Escrow contract
+    * @param _new New address of the Escrow contract
+    */
+    event SetEscrowContractRef(address _from, address _old, address _new);
 
     /* MODIFIERS */
 
-    /// @dev OpenZeppelin Escrow: Modifier used to check whether the sender has a deposit to withdraw 
-    modifier hasBalance() {
-        require(depositsOf(msg.sender) > 0, "Not enough balance");
-        _;
-    }
-
     /// @dev Modifier used to assess that the caller is the interpreter contract instance 
     modifier onlyInterpreter() {
-        require(msg.sender==address(interpreterInstance), "Caller is not the Interpreter");
+        require(msg.sender==address(interpreterContractRef), "Caller is not the Interpreter");
         _;
     }
 
     /* PUBLIC INTERFACE */
-
-    /**
-    * @dev Constructor used to instantiate the Escrow contract and set the InterpreterInstance reference
-    * @param _address Address of the Interpreter contract
-    */
-    constructor(address _address) {
-        escrow = new Escrow();
-        interpreterInstance = Interpreter(_address);
-    }
     
     /**
-    * @dev Sets the Interpreter reference. Emits a SetInterpreterInstance event
+    * @dev Sets the Interpreter contract reference. Emits a SetInterpreterInstance event
     * @param _new Address of the Interpreter contract
     */
-    function setInterpreterInstance(address _new) public onlyOwner {
-        address old = address(interpreterInstance);
-        interpreterInstance = Interpreter(_new);
-        emit SetInterpreterInstance(msg.sender, old, _new);
+    function setInterpreterContractRef(address _new) public onlyOwner {
+        address old = address(interpreterContractRef);
+        interpreterContractRef = Interpreter(_new);
+        emit SetInterpreterContractRef(msg.sender, old, _new);
     }
     
-    /* Instructions definitions */
-
-    // TODO: TESTING - Remove this function and the corresponding TCs
-    function _test(bool _value) public payable onlyInterpreter returns(bool)  {
-        return _value;
+    /**
+    * @dev Sets the Escrow instance. Emits a SetEscrowInstance event
+    * @param _new Address of the Escrow contract
+    */
+    function setEscrowContractRef(address _new) public onlyOwner {
+        address old = address(escrowInstance);
+        escrowInstance = Escrow(_new);
+        emit SetEscrowContractRef(msg.sender, old, _new);
     }
+
+    /* Instructions definitions */
 
     /**
     * @dev Checks if the addresses in parameters are equal
     * @param _address1 First address used in the comparison
     * @param _address2 Second address used in the comparison
     */
-    function _ifAddress(address _address1, address _address2) public onlyInterpreter returns (bool) {
-        emit _CALLBACK(_address1, _address2, 777);
+    function _ifAddress(address _address1, address _address2) public view onlyInterpreter returns (bool) {
         return (_address1 == _address2);
     }
 
     /**
-    * @dev Deposits all the msg.value in the corresponding Escrow address
-    * @param _to Address where to deposit funds
-    */
-    function _transferExternal(address _to) public payable onlyInterpreter {
-        _deposit(_to, msg.value);
-    }
-
-    /* Escrow */
-    
-    /**
-     * @dev Escrow: Deposit an _amount that can be withdrawn only by the _to address
+     * @dev Escrow: Deposit msg.value to the _to address
      * @param _to The address to which we deposit ETH
-     * @param _amount The amount to deposit
      */
-    function _deposit(address _to, uint256 _amount) internal {
-        escrow.deposit{value: _amount}(_to);
+    function _transfer(address _to) public payable onlyInterpreter {
+        escrowInstance.deposit(_to);
     }
-
-    /**
-     * @dev Escrow: Returns the total amount the address _to can withdrawn
-     * @param _to The address for which we query the balance
-     */
-    function depositsOf(address _to) public view returns (uint256) {
-        return escrow.depositsOf(_to);
-    }
-
-    /**
-     * @dev Withdraw the total balance
-     * Modifiers:
-     * - hasBalance: msg.sender must have enough balance in the escrow
-     */
-    function withdraw() public hasBalance {
-        escrow.withdraw(payable(msg.sender));
-    }
-    
 }
--- test/InstructionsProvider.test.js
+++ test/InstructionsProvider.test.js
@@ -1,62 +1,62 @@
 let BN = web3.utils.BN;
-let Instructions = artifacts.require("Instructions");
 let InstructionsProvider = artifacts.require("InstructionsProvider");
-let { catchRevert } = require("./exceptionsHelpers");
-const { getMapping, isDefined, isPayable, isType } = require("./ast-helper");
-const {
-  expectEvent,  // Assertions for emitted events
-  expectRevert, // Assertions for transactions that should fail
-} = require('@openzeppelin/test-helpers');
+const { expectEvent, expectRevert } = require('@openzeppelin/test-helpers');
 
 //TODO: test only setInterpreter
 
 contract("InstructionsProvider", function (accounts) {
-  const [CEO, CHAIRMAIN, ACCOUNTANT] = accounts;
+  const [CEO, CHAIRMAIN, ACCOUNTANT, INTERPRETER] = accounts;
   const emptyAddress = "0x0000000000000000000000000000000000000000";
 
-  let instanceInstructions, instanceInstructionsProvider;
+  let instanceInstructionsProvider;
 
   before(async () => {
-    instanceInstructions = await Instructions.new();
-    instanceInstructionsProvider = await InstructionsProvider.new(emptyAddress);
+    instanceInstructionsProvider = await InstructionsProvider.new();
   });
 
-  describe("Init instructions", () => {
-    let tx;
-    
-    before(async () => {
-      await instanceInstructions.addInstruction("IF-ADDR", "4", "_ifAddress(address,address)");
-      await instanceInstructions.addInstruction("TRANSFER-E", "1", "_transferExternal(address)");
+  describe("setInterpreterContractRef", () => {
+
+    it("... only owner should be able to call, otherwise revert", async () => {
+      await expectRevert.unspecified(instanceInstructionsProvider.setInterpreterContractRef(INTERPRETER, {from:ACCOUNTANT}));
     });
 
+    it("... should emit a SetInterpreterContractRef", async () => {
+      let tx = await instanceInstructionsProvider.setInterpreterContractRef(INTERPRETER, {from:CEO});
+      expectEvent(tx, "SetInterpreterContractRef");
+    });
+
+  });
+
+  describe("setEscrowContractRef", () => {
+
+    it("... only owner should be able to call, otherwise revert", async () => {
+      await expectRevert.unspecified(instanceInstructionsProvider.setEscrowContractRef(emptyAddress, {from:ACCOUNTANT}));
+    });
+
+    it("... should emit a SetInterpreterContractRef", async () => {
+      let tx = await instanceInstructionsProvider.setEscrowContractRef(INTERPRETER, {from:CEO});
+      expectEvent(tx, "SetEscrowContractRef");
+    });
+    
+  });
+
+  describe("Test supported instructions", () => {
+        
     describe("`IF-ADDR` instruction", () => {
-      it("... should be present in Instructions contract", async () => {
-        tx = await instanceInstructions.getInstruction("IF-ADDR", {from: CEO});
+      let tx;
+
+      it("... only Interpreter should be able to call, otherwise revert", async () => {
+       await expectRevert.unspecified(instanceInstructionsProvider._ifAddress(CEO, CEO, {from: CEO}));
+      });
+
+      it("... should return true when addresses are equal", async () => {
+        tx = await instanceInstructionsProvider._ifAddress(CEO, CEO, {from: INTERPRETER});
         assert(tx[1]!='',"instruction `IF-ADDR` is not present");
       });
-  
-      it("... `instructionType` should match", () => {
-        assert.equal(4, tx[0].toNumber(), "`instructionType` is not matching");
-      });
-  
-      it("... `instructionSignature` should match", () => {
-        assert.equal("_ifAddress(address,address)", tx[1], "`instructionSignature` is not matching");
-      });
 
-    });
-
-    describe("`TRANSFER-E` instruction", () => {
-      it("... should be present in Instructions contract", async () => {
-        tx = await instanceInstructions.getInstruction("TRANSFER-E", {from: CEO});
-        assert(tx[1]!='',"instruction `TRANSFER-E` is not present");
-      });
-  
-      it("... `instructionType` should match", () => {
-        assert.equal(1, tx[0].toNumber(), "`instructionType` is not matching");
-      });
-  
-      it("... `instructionSignature` should match", () => {
-        assert.equal("_transferExternal(address)", tx[1], "`instructionSignature` is not matching");
+      it("... should return false when addresses are not equal", async () => {
+        tx = await instanceInstructionsProvider._ifAddress(CEO, ACCOUNTANT, {from: INTERPRETER});
+        assert(tx[1]!='',"instruction `IF-ADDR` is not present");
       });
 
     });
