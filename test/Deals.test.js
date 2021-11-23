a386083b346e33a48bea7b4cd3ff24e80cf9074b
Hadi Sakali <hadi.sakali@gmail.com>
2021-11-23 08:06:49 +0100

    Deals: Add tests for OpenZeppelin Ownable & Pausable

--- test/Deals.test.js
+++ test/Deals.test.js
@@ -1,8 +1,6 @@
 let BN = web3.utils.BN;
 let CommonStructs = artifacts.require("CommonStructs");
 let Deals = artifacts.require("Deals");
-let { catchRevert } = require("./exceptionsHelpers");
-const { getMapping, isDefined, isPayable, isType } = require("./ast-helper");
 const {
   expectEvent,  // Assertions for emitted events
   expectRevert, // Assertions for transactions that should fail
@@ -43,38 +41,44 @@ contract("Deals", function (accounts) {
 
       it("... should emit `CreateDeal` event", async () => {
         tx = await instanceDeals.createDeal(dealAccounts, ruleList);
-        expectEvent(tx,'CreateDeal');
-      });
-
-      it("... arg `_from` should match input", () => {
-        assert.equal(tx.logs[0].args[0],CEO,"does not match");
-      });
-
-      it("... arg `_dealId` should match input", () => {
-        assert.equal(tx.logs[0].args[1].toNumber(),0,"does not match");
+        expectEvent(tx,'CreateDeal',{_from:CEO,_dealId:new BN(0)});
       });
 
     });
 
     describe("Get the deal 0", () => {
+      let dealId=0;
       let tx;
+
+      it("... rules count should match", async () => {
+        const rulesCount = await instanceDeals.getRulesCount(dealId);
+        assert.equal(rulesCount,ruleList.length,"Rules count does not match");
+      });
+
+      it("... articles counts should match", async () => {
+        const rulesCount = await instanceDeals.getRulesCount(dealId);
+        for (i=0;i<rulesCount;i++) {
+          let articlesCount = await instanceDeals.getArticlesCount(dealId,i);
+          assert.equal(articlesCount,ruleList[i].length,"Article count does not match for rule " + i);
+        }
+      });
 
       it("... accounts should match", async () => {
         // Compare external account one by one
-        const dealAccountsCount = await instanceDeals.getAccountsCount(0);
+        const dealAccountsCount = await instanceDeals.getAccountsCount(dealId);
         for (i=0;i<dealAccountsCount.toNumber();i++) {
-          tx = await instanceDeals.getAccount(0, i);
-          assert.equal(tx,dealAccounts[i],"address should match");
+          tx = await instanceDeals.getAccount(dealId, i);
+          assert.equal(tx,dealAccounts[i],"address does not match");
         }
       });
 
       it("... rules should match", async () => {
         // Compare external account one by one
-        const rulesCount = await instanceDeals.getRulesCount(0);
+        const rulesCount = await instanceDeals.getRulesCount(dealId);
         for (i=0;i<rulesCount.toNumber();i++) {
-          const articlesCount = await instanceDeals.getArticlesCount(0,i);
+          const articlesCount = await instanceDeals.getArticlesCount(dealId,i);
           for (j=0;j<articlesCount;j++){
-            tx = await instanceDeals.getArticle(0, i, j);
+            tx = await instanceDeals.getArticle(dealId, i, j);
             assert.equal(tx.instructionName,ruleList[i][j][0],"instructionName not matching for Article(i,j)=(" + i + "," + j + ")");
             assert.equal(tx.paramStr,ruleList[i][j][1],"PparamStr not matching for Article(i,j)=(" + i + "," + j + ")");
             assert.equal(tx.paramUInt,ruleList[i][j][2],"paramUInt not matching for Article(i,j)=(" + i + "," + j + ")");
@@ -86,6 +90,46 @@ contract("Deals", function (accounts) {
       });
 
     });
+
+  });
+
+  describe("Access Control: ", () => {
+
+    describe("OpenZeppelin Ownable",() => {
+
+      it("... only owner can pause the contract", async () => {
+        await expectRevert.unspecified(instanceDeals.pause({from: ACCOUNTANT}));
+      });
+  
+      it("... only owner can unpause the contract", async () => {
+        await expectRevert.unspecified(instanceDeals.unpause({from: ACCOUNTANT}));
+      });  
+
+    });
+
+    describe("OpenZeppelin Pausable",() => {
+
+      it("... when the contract is paused, it should emit a `Paused` event", async () => {
+        let tx = await instanceDeals.pause({from:CEO});
+        expectEvent(tx, "Paused");
+      });
+
+      it("... createDeal should not be callable by anyone", async () => {
+        await expectRevert.unspecified(instanceDeals.createDeal(dealAccounts, ruleList));
+      });
+
+      it("... when the contract is unpaused, it should emit a `Unpaused` event", async () => {
+        let tx = await instanceDeals.unpause({from:CEO});
+        expectEvent(tx, "Unpaused");
+      });
+
+      it("... createDeal should then be callable again", async () => {
+        let tx = await instanceDeals.createDeal(dealAccounts, ruleList);
+        expectEvent(tx,"CreateDeal");
+      });
+
+    });
+
   });
 
 });
